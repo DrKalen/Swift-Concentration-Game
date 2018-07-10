c47e7af056e14090a8cadc33f57b3405cb499cb3
Kalen Hammann <kalen7@gmail.com>
2018-07-10 11:56:05 -0400

    Files added via upload (restarted project)
    Get Concentration Game working with 4 cards

---
+++ Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Kalen Hammann on 7/9/18.
+//  Copyright © 2018 Human Family. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
---
+++ Concentration.swift
@@ -0,0 +1,46 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Kalen Hammann on 7/9/18.
+//  Copyright © 2018 Human Family. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                    //turn all cards face down
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        //TODO: SHUFFLE THE CARDS
+    }
+}
