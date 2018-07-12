fa124fe3ac2314c51dffe111775c8a831fe26de5
Kalen Hammann <kalen7@gmail.com>
2018-07-12 12:33:40 -0400

    Change to computed properties for numberOfPairsOfCards and indexOfTheOneAndOnlyFaceUpCard

--- Concentration.swift
+++ Concentration.swift
@@ -12,7 +12,26 @@ class Concentration
 {
     var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
         if !cards[index].isMatched {
